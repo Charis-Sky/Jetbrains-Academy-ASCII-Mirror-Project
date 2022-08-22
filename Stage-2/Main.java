5f1294ec75a0df724d50376c5a90c9d2f2681baf
Charis-Sky <xaris.oyr@gmail.com>
2022-08-22 14:38:07 +0300

    Added Stage 2

---
+++ Stage-2/Main.java
@@ -0,0 +1,17 @@
+package asciimirror;
+import java.util.Scanner;
+
+public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Input the file path:");
+        System.out.println(scanner.nextLine());
+        System.out.println("""
+                            ^__^
+                    _______/(oo)
+                /\\/(       /(__)
+                   | w----||   \s
+                   ||     ||   \s""");
+    }
+}
