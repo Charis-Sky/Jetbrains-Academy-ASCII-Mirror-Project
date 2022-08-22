e945c84964186e6612eff4a953a34256564d81ff
Charis-Sky <xaris.oyr@gmail.com>
2022-08-22 14:58:59 +0300

    Added Stage 3

---
+++ Stage-3/Main.java
@@ -0,0 +1,24 @@
+package asciimirror;
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Input the file path:");
+        String path = scanner.nextLine();
+
+        try{
+            File animal = new File(path);
+            Scanner fileScanner = new Scanner(animal);
+            while (fileScanner.hasNext()) {
+                System.out.println(fileScanner.nextLine());
+            }
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+
+    }
+}
